47f52b84d2478393036cc38f1f6b8be549914925
ponychan <rzsckbmx@163.com>
2017-10-06 12:09:41 +0800

    3.13.cpp
    传值与传引用的比较

--- ConsoleApplication3_13/ConsoleApplication313.cpp
+++ ConsoleApplication3_13/ConsoleApplication313.cpp
@@ -1,11 +1,30 @@
 // ConsoleApplication313.cpp: 定义控制台应用程序的入口点。
-//
+//值传递与引用传递的比较
 
 #include "stdafx.h"
+#include<iostream>
+#include<iomanip>
+using namespace std;
 
+//子函数
+void fiddle(int in1, int in2) {
+	in1 = in1 + 100;
+	in2 = in2 + 100;
+	cout << "The values are ";
+	cout << setw(5) << in1;
+	cout << setw(5) << in2 << endl;
+}
 
 int main()
 {
-    return 0;
+	int v1 = 7, v2 = 12;
+	cout << "The values are ";
+	cout << setw(5) << v1;
+	cout << setw(5) << v2 << endl;
+	fiddle(v1, v2);
+	cout << "The values are ";
+	cout << setw(5) << v1;
+	cout << setw(5) << v2 << endl;
+	return 0;
 }
 
